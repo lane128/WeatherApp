e7459b8c8c7d1bfbaf9fd9e84fb9f0cf29d8bfa6
Wang Lei <lane128@gmail.com>
2015-08-19 02:17:03 +0800

    Weather app location init

--- WeatherApp/ViewController.m
+++ WeatherApp/ViewController.m
@@ -7,21 +7,61 @@
 //
 
 #import "ViewController.h"
+@import CoreLocation;
 
-@interface ViewController ()
-
+@interface ViewController()<CLLocationManagerDelegate>
+@property (strong ,nonatomic) CLLocationManager *locationManager;
+@property (nonatomic) double systemVersion;
 @end
 
 @implementation ViewController
-
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    //init the locationManager and set the accuracy
+    self.locationManager=[[CLLocationManager alloc] init];
+    
+    //set the locationManager delegate to the class(#important!)
+    //#need add 'NSLocationAlwaysUsageDescription' in Info.plist file.#//
+    //#or the notification to use location would not show!!#//
+    self.locationManager.delegate=self;
+    
+    self.locationManager.desiredAccuracy=kCLLocationAccuracyBest;
+    if ([self isIOS8]) {
+        [self.locationManager requestAlwaysAuthorization];
+    }
+    [self.locationManager startUpdatingLocation];
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void) locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations{
+    CLLocation *theLocation=locations[locations.count-1];
+    NSLog(@"%f",theLocation.horizontalAccuracy);
+    if (theLocation.horizontalAccuracy>0) {
+        NSLog(@"%f   %f",theLocation.coordinate.latitude,theLocation.coordinate.longitude);
+        [self.locationManager stopUpdatingLocation];
+    }
+}
+
+-(void) locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error{
+    NSLog(@"%@",error);
+}
+
+//check the device system version
+- (BOOL) isIOS8{
+    UIDevice *myDevice=[UIDevice currentDevice];
+    self.systemVersion=[myDevice.systemVersion doubleValue];
+    NSLog(@"systemVersion is %@",myDevice.systemVersion);
+    if (self.systemVersion>8.0) {
+        return true;
+    }else{
+        return false;
+    }
 }
 
 @end
